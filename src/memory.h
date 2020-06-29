7ef9c8e54a9969504702214d1046e2940efd9385
Artemoire <artemoire@github.com>
2020-06-29 04:32:56 +0200

    19 - Strings (done)

--- src/memory.h
+++ src/memory.h
@@ -1,6 +1,9 @@
 #ifndef clox_memory_h
 #define clox_memory_h
 
+#include "common.h"
+#include "object.h"
+
 #define ALLOCATE(type, count) \
     (type*)reallocate(NULL, 0, sizeof(type) * (count))
 
@@ -11,9 +14,12 @@
     (type*)reallocate(previous, sizeof(type) * (oldCount), \
         sizeof(type) * (count))
 
+#define FREE(type, pointer) reallocate(pointer, sizeof(type), 0)
+
 #define FREE_ARRAY(type, pointer, oldCount) \
     reallocate(pointer, sizeof(type) * (oldCount), 0)
 
 void* reallocate(void* previous, size_t oldSize, size_t newSize);
+void freeObjects();
 
 #endif
